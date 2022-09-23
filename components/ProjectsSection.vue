cd07510c8ffb5f79dc352226247373e4d626eb60
arpado <ozsvar.arpad@gmail.com>
2022-09-23 19:48:50 +0200

    title added, background modified, minor style changes

--- components/ProjectsSection.vue
+++ components/ProjectsSection.vue
@@ -1,33 +1,50 @@
 <template>
-  <div class="projects-container ">
-      <ProjectElement v-for="(page, index) in pages" :key="index" :page="page"/>
+  <div class="projects-section">
+    <h2>Some of my projects so far:</h2>
+    <hr />
+    <div class="projects-container">
+      <ProjectElement
+        v-for="(page, index) in pages"
+        :key="index"
+        :page="page"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 // section-unit flex-c-c
-import pages from '@/content/webpages.json'
+import pages from "@/content/webpages.json";
 export default {
-    data () {
-        return {
-            pages
-        }
-    },
-}
+  data() {
+    return {
+      pages,
+    };
+  },
+};
 </script>
 
 <style scoped>
+.projects-section {
+  width: 100vw;
+  min-height: 100vh;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+}
 .projects-container {
-    width: 100vw;
-    min-height: 100vh;
-    padding-left: 1rem;
-    padding-right: 1rem;
-    height: fit-content;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-content: center;
-    gap: 2rem;
-    background-color: black;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: center;
+  gap: 2rem;
+  /* background-color: black; */
+}
+hr {
+  width: 25%;
 }
 </style>
