ba0a86dfb5e55c7c1ac4e3c0be25ebca16593425
arpado <ozsvar.arpad@gmail.com>
2022-09-23 19:45:28 +0200

    about section added, re-styling

--- components/ContactElement.vue
+++ components/ContactElement.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="contact-section">
-    <div data-augmented-ui="all-hex-alt border" id="hexagon-2"></div>
     <form
       class="email-form"
       action="https://formsubmit.co/2504b17c1cdcf59e3fd818d8e727da33"
       method="POST"
       name="”EmailForm”"
     >
+      <h2>Contacts</h2>
+      <hr />
       <label for="name">
         <p>Name</p>
         <input class="message-input" type="text" name="name" required />
@@ -33,6 +34,40 @@
         Send Message
       </div>
     </form>
+    <div class="about">
+      <h2>About me</h2>
+      <hr />
+      <h3>Who am I?</h3>
+      <p>
+        I am on of the many self-thought programmers, who took the chance to
+        learn the ropes of coding during the Great Pandemics.
+      </p>
+      <hr />
+      <h3>What do I do?</h3>
+      <p>
+        I'm proficient with the Vue ecosystem (Vue3, Nuxt3, Pinia and Router),
+        but as web development relies heavily on plain old JavaScript, I have
+        aquired a deep understanding of HTML-CSS-JavaScript too, plus a little
+        bit of MySQL and I'm also familiar with Git, npm, bash, algorithms, data
+        structures.
+      </p>
+      <hr />
+      <h4>How may I help you?</h4>
+      <p>
+        I can deliver anything ranging from one page websites to small web
+        applications, and make your site fully responsive and indexed high on
+        the list of search engines with SEO. When you are satisfied I can even
+        host and maintain your website for you.
+      </p>
+      <hr />
+      <p>
+        You can find my resume
+        <a href="/pdf/arpad-ozsvar-cv.pdf" target="_blank">HERE</a>.
+      </p>
+    </div>
+    <div class="hexagon-container">
+      <div data-augmented-ui="all-hex-alt border" id="hexagon-2"></div>
+    </div>
   </div>
 </template>
 
@@ -55,22 +90,26 @@ export default {
 <style scoped>
 .contact-section {
   width: 100vw;
-  height: 100vh;
+  height: fit-content;
   background-color: black;
-  position: relative;
+  /* position: relative; */
   overflow: hidden;
   display: flex;
-  justify-content: flex-start;
+  flex-direction: column-reverse;
+  justify-content: space-between;
   align-items: center;
-  position: relative;
+  gap: 1rem;
+  padding: 1rem;
+  /* position: relative; */
 }
-.email-form {
+.email-form,
+.about {
   height: fit-content;
   min-height: 80vh;
   min-width: 300px;
   width: 50vw;
-  max-width: 500px;
-  margin-left: 10vw;
+  max-width: 400px;
+  /* margin-left: 10vw; */
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
@@ -78,7 +117,7 @@ export default {
 }
 input,
 textarea {
-  width: 250px;
+  width: 300px;
   margin-top: 10px;
 }
 .submit-btn {
@@ -90,13 +129,39 @@ textarea {
   justify-content: center;
   align-items: center;
 }
+hr {
+  width: 50%;
+}
+.hexagon-container {
+  width: 400px;
+  height: fit-content;
+  overflow: hidden;
+  display: none;
+}
 #hexagon-2 {
-  position: absolute;
-  bottom: 15vh;
-  right: -15rem;
+  /* position: absolute; */
+  /* bottom: 15vh;
+  right: -15rem; */
   --aug-all-width: 80vh;
   --aug-inlay-bg: black;
   --aug-border-bg: yellow;
   --aug-border-all: 60px;
+}
+@media (min-width: 650px) {
+  .contact-section {
+  height: 90vh;
+  flex-direction: row;
+  }
+}
+@media (min-width: 750px) {
+  .hexagon-container {
+    display: unset;
+  }
+}
+@media (min-width: 850px) {
+ .email-form,
+.about {
+  min-width: 300px;
+  }
 }
 </style>
