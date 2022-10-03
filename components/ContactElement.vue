51446ea2625a4e40a4fb81f84710a8c92d900316
arpado <ozsvar.arpad@gmail.com>
2022-10-03 21:57:24 +0200

    style and order changed for better UX

--- components/ContactElement.vue
+++ components/ContactElement.vue
@@ -1,7 +1,42 @@
 <template>
   <div class="contact-section">
+    <div class="hexagon-container">
+      <div data-augmented-ui="all-hex-alt border" id="hexagon-2"></div>
+    </div>
+    <div class="about fade-in" id="about">
+      <h2>About me</h2>
+      <hr />
+      <h3>Who am I?</h3>
+      <p>
+        I am on of the many self-thought programmers, who took the chance to
+        learn the ropes of coding during the Great Pandemics.
+      </p>
+      <hr />
+      <h3>What do I do?</h3>
+      <p>
+        I'm proficient with the Vue ecosystem (Vue3, Nuxt3, Pinia and Router),
+        but as web development relies heavily on plain old JavaScript, I have
+        aquired a deep understanding of HTML-CSS-JavaScript too, plus a little
+        bit of MySQL and I'm also familiar with Git, npm, bash, algorithms, data
+        structures.
+      </p>
+      <hr />
+      <h4>How may I help you?</h4>
+      <p>
+        I can deliver anything ranging from one page websites to small web
+        applications, and make your site fully responsive and indexed high on
+        the list of search engines with SEO. When you are satisfied I can even
+        host and maintain your website for you.
+      </p>
+      <hr />
+      <p>
+        You can find my resume
+        <a href="/pdf/arpad-ozsvar-cv.pdf" target="_blank">here</a>.
+      </p>
+    </div>
     <form
       class="email-form fade-in"
+      id="email-form"
       action="https://formsubmit.co/2504b17c1cdcf59e3fd818d8e727da33"
       method="POST"
       name="”EmailForm”"
@@ -34,40 +69,6 @@
         Send Message
       </div>
     </form>
-    <div class="about fade-in">
-      <h2>About me</h2>
-      <hr />
-      <h3>Who am I?</h3>
-      <p>
-        I am on of the many self-thought programmers, who took the chance to
-        learn the ropes of coding during the Great Pandemics.
-      </p>
-      <hr />
-      <h3>What do I do?</h3>
-      <p>
-        I'm proficient with the Vue ecosystem (Vue3, Nuxt3, Pinia and Router),
-        but as web development relies heavily on plain old JavaScript, I have
-        aquired a deep understanding of HTML-CSS-JavaScript too, plus a little
-        bit of MySQL and I'm also familiar with Git, npm, bash, algorithms, data
-        structures.
-      </p>
-      <hr />
-      <h4>How may I help you?</h4>
-      <p>
-        I can deliver anything ranging from one page websites to small web
-        applications, and make your site fully responsive and indexed high on
-        the list of search engines with SEO. When you are satisfied I can even
-        host and maintain your website for you.
-      </p>
-      <hr />
-      <p>
-        You can find my resume
-        <a href="/pdf/arpad-ozsvar-cv.pdf" target="_blank">HERE</a>.
-      </p>
-    </div>
-    <div class="hexagon-container">
-      <div data-augmented-ui="all-hex-alt border" id="hexagon-2"></div>
-    </div>
   </div>
 </template>
 
@@ -109,7 +110,7 @@ export default {
   /* position: relative; */
   overflow: hidden;
   display: flex;
-  flex-direction: column-reverse;
+  flex-direction: column;
   justify-content: space-between;
   align-items: center;
   gap: 1rem;
@@ -165,7 +166,7 @@ hr {
 @media (min-width: 650px) {
   .contact-section {
     height: 90vh;
-    flex-direction: row;
+    flex-direction: row-reverse;
   }
 }
 @media (min-width: 750px) {
