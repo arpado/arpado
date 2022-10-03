4019384ee5c6af4778be91d72d9a32994ee36a78
arpado <ozsvar.arpad@gmail.com>
2022-10-03 21:46:52 +0200

    menu added

--- app.vue
+++ app.vue
@@ -1,21 +1,52 @@
 <template>
-  <div class="maim-container">
-    <HeroSection />
-    <StorySection />
-    <ProjectsSection />
-    <ContactElement />
-    <FooterElement />
-    <ModalView />
+  <div>
+    <div class="maim-container" v-if="pageLoaded">
+      <VerticalMenu />
+      <HeroSection />
+      <StorySection />
+      <ProjectsSection />
+      <ContactElement />
+      <FooterElement />
+      <ModalView />
+    </div>
+      <!-- <div > -->
+    <transition name="fade" mode="in-out">
+        <LoaderElement class="loader" v-if="!pageLoaded" />
+    </transition>
+      <!-- </div> -->
   </div>
 </template>
 
 <script>
 import gsap from "gsap";
 // import { faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons'
+// name="loader-fade">
+// @before-leave="loaderLeave"
 
 export default {
-  
-}
+  data() {
+    return {
+      pageLoaded: false,
+    };
+  },
+  methods: {
+    loaderLeave(el) {
+      console.log(el)
+      gsap.to(el, done, {
+        opacity: 0,
+        duration: 1,
+        done: this.setPageLoad
+      })
+    },
+    setPageLoad() {
+      this.pageLoaded = true
+    },
+  },
+  mounted() {
+    // this.loaderLeave('.loader');
+    this.pageLoaded = true;
+  },
+};
 </script>
 
 <style>
@@ -26,6 +57,7 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  scroll-behavior: smooth;
 }
 :root {
   --mainYellow: yellow;
@@ -33,8 +65,8 @@ export default {
 body {
   color: white;
   overflow-x: hidden;
-  font-family: 'Times New Roman', Times, serif;
-  font-family: 'Roboto';
+  font-family: "Times New Roman", Times, serif;
+  font-family: "Roboto";
   text-shadow: 0 0 5px black;
   background: black;
   background-image: url("/images/black-sand-bg.jpeg");
@@ -42,7 +74,9 @@ body {
   background-size: cover;
   background-attachment: fixed;
 }
-
+a {
+  color: yellow;
+}
 /* general styles */
 
 .section-unit {
@@ -58,15 +92,16 @@ body {
   align-items: center;
 }
 .yellow-btn {
-    color: black;
-    background-color: yellow;
-    border-radius: 5px;
-    transition: 0.25s ease-out;
-    text-shadow: none;
+  color: black;
+  background-color: yellow;
+  border: 2px solid black;
+  border-radius: 5px;
+  transition: 0.25s ease-out;
+  text-shadow: none;
 }
 .yellow-btn:hover {
-    cursor: pointer;
-    box-shadow: 0 0 20px 2px yellow;
+  cursor: pointer;
+  box-shadow: 0 0 20px 2px yellow;
 }
 .yellow-btn > p {
   text-align: center;
@@ -81,5 +116,28 @@ body {
 ::-webkit-scrollbar-thumb {
   background: var(--mainYellow);
   border-radius: 5px;
+}
+/* .loader-fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.loader-fade-leave-to {
+  opacity: 0;
+} */
+/* .v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+} */
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 1s linear;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
