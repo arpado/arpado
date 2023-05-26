523e0a2435061985815e5b22045a3b71e48d618a
arpado <ozsvar.arpad@gmail.com>
2023-05-26 16:43:24 +0200

    styles changed

--- components/FooterElement.vue
+++ components/FooterElement.vue
@@ -1,26 +1,29 @@
 <template>
-  <div class="footer-container">
-    <div class="link-bar">
-      <!-- <a href="facebook.com">
+  <div class="footer-container-outer">
+    <div class="footer-container-inner">
+      <div class="link-bar">
+        <!-- <a href="facebook.com">
         <font-awesome-icon icon="fa-brands fa-facebook-f" />
       </a> -->
-      <a
-        href="https://www.linkedin.com/in/%C3%A1rp%C3%A1d-ozsv%C3%A1r-774482251/"
-        target="_blank"
-      >
-        <font-awesome-icon icon="fa-brands fa-linkedin-in" />
-      </a>
-      <!-- <font-awesome-icon icon="fa-brands fa-google-plus-g" /> -->
-      <a href="https://github.com/arpado" target="_blank">
-        <font-awesome-icon icon="fa-brands fa-github" />
-      </a>
-      <!-- <font-awesome-icon :icon="['fa', 'facebook-f']" class="icon alt"/> -->
-      <!-- <font-awesome-icon :icon="['fab', 'github']" class="icon alt"/> -->
-      <!-- <a href=""><img class="icon" src="/assets/font-awesome/github.svg" alt=""></a> -->
-    </div>
-    <div class="copyright">
-      <p>Made by Arpad O.</p>
-      <p>Copyright, 2022.</p>
+        <a
+          href="https://www.linkedin.com/in/%C3%A1rp%C3%A1d-ozsv%C3%A1r-774482251/"
+          target="_blank"
+        >
+          <font-awesome-icon icon="fa-brands fa-linkedin-in" />
+        </a>
+        <!-- <font-awesome-icon icon="fa-brands fa-google-plus-g" /> -->
+        <a href="https://github.com/arpado" target="_blank">
+          <font-awesome-icon icon="fa-brands fa-github" />
+        </a>
+        <!-- <font-awesome-icon :icon="['fa', 'facebook-f']" class="icon alt"/> -->
+        <!-- <font-awesome-icon :icon="['fab', 'github']" class="icon alt"/> -->
+        <!-- <a href=""><img class="icon" src="/assets/font-awesome/github.svg" alt=""></a> -->
+      </div>
+      <div class="copyright">
+        <span>Made by Arpad O.</span>
+        <br />
+        <span>Copyright, 2022.</span>
+      </div>
     </div>
   </div>
 </template>
@@ -36,11 +39,17 @@ export default {};
 </script>
 
 <style>
-.footer-container {
+.footer-container-outer {
   width: 100%;
-  height: 10vh;
-  padding: 1rem;
   background-color: black;
+  padding: 16px 5%;
+}
+.footer-container-inner {
+  width: 100%;
+  max-width: 1800px;
+  /* height: 10vh; */
+  /* padding: 1rem; */
+  margin: 0 auto;
   /* border: 1px solid white; */
   display: flex;
   justify-content: space-between;
