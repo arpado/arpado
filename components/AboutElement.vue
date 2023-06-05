dc75c48cd9cdd3bc5c673c4823bc796ce25a48ed
arpado <ozsvar.arpad@gmail.com>
2023-06-05 14:32:48 +0200

    content modified

--- components/AboutElement.vue
+++ components/AboutElement.vue
@@ -2,18 +2,25 @@
   <div>
     <h2>About me</h2>
     <hr />
-    <p>
+    <!-- <p>
       I am one of the many self-taught programmers who seized the opportunity to
       delve into coding and web development during the Great Pandemics.
-    </p>
-    <p>
+    </p> -->
+        <p>Hi there!</p>
+        <p>
+          My name is Arpad, and I'm a passionate beginner web developer eager to bring your ideas to
+          life. I specialize in creating responsive, user-friendly websites that
+          are both visually appealing and functional.
+        </p>
+
+    <!-- <p>
       My proficiency lies within the Vue ecosystem, encompassing Vue3, Nuxt3,
       Pinia, and Router. However, since web development heavily relies on the
       foundation of HTML, CSS, and JavaScript, I have acquired a profound
       understanding of these technologies as well. In addition, I have a working
       knowledge of MySQL, Git, npm, bash scripting, algorithms, and data
       structures.
-    </p>
+    </p> -->
     <p>
       I offer a wide range of services, including the creation of one-page
       websites and small web applications. I excel in crafting fully responsive
@@ -21,6 +28,13 @@
       effective SEO techniques. If you're satisfied with the final product, I
       can even handle website hosting and maintenance for you.
     </p>
+            <p>
+          Whether you're looking to establish your online presence, improve your
+          website's performance, or bring a new project to life, I'm committed
+          to working closely with you to understand your unique needs and
+          deliver a product that exceeds your expectations.
+        </p>
+        <p>Let's build something great together!</p>
     <hr />
     <p>
       You can find my resume
